a96eaed88bf119f5ea6bbb753036efb59830eb9e
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2021-03-07 09:50:01 -0300

    first commit

---
+++ Entity/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Composition.Entity {
+    class Client {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client() {
+        }
+
+        public Client(string name, string email, DateTime birthDate) {
+            this.Name = name;
+            this.Email = email;
+            this.BirthDate = birthDate;
+        }
+
+        public override string ToString() {
+            return
+                this.Name
+                + ", ("
+                + this.BirthDate.ToString("dd/MM/yyyy")
+                + ") - "
+                + this.Email;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Exemplo de composição de Classes em C#
+
+- Entidades:
+    [x] Client
+    [x] Product
+    [x] Order
+    [x] OrderItem
+
+- Enum:
+    [x] OrderStatus
